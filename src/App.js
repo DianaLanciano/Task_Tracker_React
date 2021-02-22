eeb20f4cd5c58760aba3b820989db6274d4e8dcf
Diana <isakovdiana1@gmail.com>
2021-02-22 12:56:58 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -26,22 +26,26 @@ function App() {
      return data;
   }
 
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:3000/tasks/${id}`);
+    const data = await res.json();
+     return data;
+  }
+
+
 
   const addTask = async (task) =>{
-    const res = fetch('http://localhost:3000/tasks',
+    const res = await fetch('http://localhost:3000/tasks',
     {method:'POST',
     headers:{'Content-type': 'application/json'
   },
     body: JSON.stringify(task)
 });
 
-  const data = JSON.parse(res);
+  const data = await (await res).json();
+  console.log("data is  "   + data);
   setTasks([...tasks, data]);
 
-
-      // const id = Math.floor(Math.random()*1000) + 1;
-      // const newTask = {id, ...task};
-      // setTasks([...tasks, newTask]);
   }
 
   const deleteTask = async (id) => {
@@ -49,9 +53,23 @@ function App() {
     setTasks(tasks.filter( task => (task.id !== id)));
   }
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder};
+
+    const res = await fetch(`http://localhost:3000/tasks/${id}`,
+    {method:'PUT',
+    headers:{'Content-type': 'application/json'
+  }, 
+  //converts a JavaScript object or value to a JSON string,
+   body: JSON.stringify(updatedTask)
+  });
+
+  const data = await (await res).json();
+
     setTasks(tasks.map( task => (task.id === id
-      ?{ ...task, reminder: !task.reminder
+      ?{ ...task, reminder: !data.reminder
       } : task)));
   }
 
